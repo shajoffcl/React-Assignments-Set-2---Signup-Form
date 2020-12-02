d56b6b7d11681d78b344971d32f34eb67a7f1ed9
Mohd Shaib <shaj.native@gmail.com>
2020-12-03 01:08:25 +0530

    initial setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState("All fields are mandatory");
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1>{errorMessage}</h1>
+      <input type="text" data-testid="name" placeholder="Enter name" />
+      <br />
+      <input type="text" data-testid="email" placeholder="Enter email" />
+      <br />
+      <input type="text" data-testid="gender" placeholder="Enter gender" />
+      <br />
+      <input
+        type="text"
+        data-testid="phonenumber"
+        placeholder="Enter phone number"
+      />
+      <br />
+      <input
+        type="password"
+        data-testid="password"
+        placeholder="Enter password"
+      />
+      <br />
+      <button data-testid="submit">Submit</button>
+    </div>
+  );
+};
 
 export default App;
