f28d147dfcdd5190ddd8cc9ab6b658790fc490cf
Mohd Shaib <shaj.native@gmail.com>
2020-12-03 01:19:05 +0530

    form added

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,28 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState("All fields are mandatory");
   return (
     <div id="main">
-      <h1>{errorMessage}</h1>
-      <input type="text" data-testid="name" placeholder="Enter name" />
-      <br />
-      <input type="text" data-testid="email" placeholder="Enter email" />
-      <br />
-      <input type="text" data-testid="gender" placeholder="Enter gender" />
-      <br />
-      <input
-        type="text"
-        data-testid="phonenumber"
-        placeholder="Enter phone number"
-      />
-      <br />
-      <input
-        type="password"
-        data-testid="password"
-        placeholder="Enter password"
-      />
-      <br />
-      <button data-testid="submit">Submit</button>
+      <h3 style={{ color: "red" }}>{errorMessage}</h3>
+      <form>
+        <input type="text" data-testid="name" placeholder="Enter name" />
+        <br />
+        <input type="text" data-testid="email" placeholder="Enter email" />
+        <br />
+        <input type="text" data-testid="gender" placeholder="Enter gender" />
+        <br />
+        <input
+          type="text"
+          data-testid="phonenumber"
+          placeholder="Enter phone number"
+        />
+        <br />
+        <input
+          type="password"
+          data-testid="password"
+          placeholder="Enter password"
+        />
+        <br />
+        <button data-testid="submit">Submit</button>
+      </form>
     </div>
   );
 };
